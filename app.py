ec0823f8e60c20f4361268b0c890fc416f9c3d2e
Ahmed Ragab <113994567+ahmed17717@users.noreply.github.com>
2025-09-08 14:39:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import joblib
 
 # Load model and vectorizer
-model = joblib.load("../models/sentiment_model.pkl")
-vectorizer = joblib.load("../models/vectorizer.pkl")
+model = joblib.load("models/sentiment_model.pkl")
+vectorizer = joblib.load("models/vectorizer.pkl")
 
 st.title("🎬 IMDb Sentiment Analysis")
 st.write("Type a movie review and find out if it's Positive or Negative!")
@@ -19,3 +19,4 @@ if st.button("Predict Sentiment"):
         prediction = model.predict(review_vec)[0]
         sentiment = "🌟 Positive" if prediction == 1 else "💔 Negative"
         st.success(f"Prediction: {sentiment}")
+
