479325ac2712565adb52b00c55e2070a40827be1
Ahmed Ragab <113994567+ahmed17717@users.noreply.github.com>
2025-09-08 15:36:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import joblib
-import requests
 import io
+from huggingface_hub import hf_hub_download
 
-# Load model and vectorizer
-def load_from_hf(url):
-    response = requests.get(url)
-    return joblib.load(io.BytesIO(response.content))
-    
-model = load_from_hf("https://huggingface.co/ahmed159260/imdb-sentiment-analysis-model/resolve/main/sentiment_model.pkl")
-vectorizer = load_from_hf("https://huggingface.co/ahmed159260/imdb-sentiment-analysis-model/resolve/main/vectorizer.pkl")
+# Download model from Hugging Face Hub
+model_path = hf_hub_download(
+    repo_id="ahmed159260/imdb-sentiment-analysis-model",
+    filename="sentiment_model.pkl"
+)
+vectorizer_path = hf_hub_download(
+    repo_id="ahmed159260/imdb-sentiment-analysis-model",
+    filename="vectorizer.pkl"
+)
+
+# Load them
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
 
 st.title("🎬 IMDb Sentiment Analysis")
 st.write("Type a movie review and find out if it's Positive or Negative!")
@@ -25,6 +31,7 @@ if st.button("Predict Sentiment"):
         prediction = model.predict(review_vec)[0]
         sentiment = "🌟 Positive" if prediction == 1 else "💔 Negative"
         st.success(f"Prediction: {sentiment}")
+
 
 
 
